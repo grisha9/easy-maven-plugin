6615034d041b0e5f30be0c0810e402eef3d6b080
gmyasoedov <gmyasoedov@yandex.ru>
2025-10-30 21:38:45 +0300

    chore: run tests #38

--- gmaven/src/main/kotlin/ru/rzn/gmyasoedov/gmaven/project/process/GOSProcessHandler.kt
+++ gmaven/src/main/kotlin/ru/rzn/gmyasoedov/gmaven/project/process/GOSProcessHandler.kt
@@ -7,15 +7,19 @@ import com.intellij.execution.process.ProcessListener
 import com.intellij.openapi.util.Key
 import com.intellij.openapi.util.registry.Registry
 import com.intellij.openapi.util.text.StringUtil
+import ru.rzn.gmyasoedov.gmaven.GMavenConstants
 import ru.rzn.gmyasoedov.gmaven.server.GServerRequest
 
 class GOSProcessHandler(
     private val request: GServerRequest,
-    private val commandLine: GeneralCommandLine,
+    commandLine: GeneralCommandLine,
     private val processConsumer: ((process: GOSProcessHandler) -> Unit)? = null
 ) : OSProcessHandler(commandLine) {
 
     init {
+        if (request.debugPort != null && request.settings.env[GMavenConstants.GMAVEN_ENV_DEBUG_PORT] != null) {
+            addListeningRemoteAddress(request.debugPort)
+        }
         addProcessListener(object : ProcessListener {
             override fun onTextAvailable(event: ProcessEvent, outputType: Key<*>) {
                 super.onTextAvailable(event, outputType)
@@ -23,11 +27,20 @@ class GOSProcessHandler(
                 if (Registry.`is`("gmaven.server.debug")) {
                     println(text)
                 }
-                if (request.listener != null) {
-                    request.listener.onTaskOutput(request.taskId, text, true)
+                request.listener?.onTaskOutput(request.taskId, text, true)
+                if (request.debugPort != null && text.contains("T E S T S")) {
+                    addListeningRemoteAddress(request.debugPort)
                 }
             }
         })
+    }
+
+    private fun addListeningRemoteAddress(debugPort: Int) {
+        request.listener?.onTaskOutput(
+            request.taskId,
+            "Easy Maven: Listening for transport dt_socket at address: $debugPort" + System.lineSeparator(),
+            true
+        )
     }
 
     fun startAndWait() {
