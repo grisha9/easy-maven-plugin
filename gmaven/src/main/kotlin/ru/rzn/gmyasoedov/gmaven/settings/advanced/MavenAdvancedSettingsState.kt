6ecb4fd8ac8c958962b997565173b68ec80704e6
gmyasoedov <gmyasoedov@yandex.ru>
2025-12-01 09:30:39 +0300

    feat: Maven Central Web File Provider

---
+++ gmaven/src/main/kotlin/ru/rzn/gmyasoedov/gmaven/settings/advanced/MavenAdvancedSettingsState.kt
@@ -0,0 +1,22 @@
+package ru.rzn.gmyasoedov.gmaven.settings.advanced
+
+import com.intellij.openapi.components.*
+
+const val DEFAULT_SEARCH_URL = "https://search.maven.org/"
+
+@Service(Service.Level.APP)
+@State(name = "EasyMavenAdvancedSettings", storages = [Storage("EasyMavenAdvancedSettings.xml")])
+class MavenAdvancedSettingsState : PersistentStateComponent<MavenAdvancedSettingsState>, BaseState() {
+    var searchUrl by string(DEFAULT_SEARCH_URL)
+    var addGroupIdToSearch by property(false)
+    var searchInSplitWindow by property(true)
+    var groupIdFolderNavigation by property(true)
+
+    override fun getState(): MavenAdvancedSettingsState = this
+
+    override fun loadState(state: MavenAdvancedSettingsState) = this.copyFrom(state)
+
+    companion object {
+        fun getInstance(): MavenAdvancedSettingsState = service()
+    }
+}
