d8b77c7ec819e14d39af95c4388e5435c913ec4e
gmyasoedov <gmyasoedov@yandex.ru>
2025-12-23 17:24:55 +0300

    fix: Schema location #56

--- gmaven/src/main/kotlin/ru/rzn/gmyasoedov/gmaven/dom/GMavenSchemaProvider.kt
+++ gmaven/src/main/kotlin/ru/rzn/gmyasoedov/gmaven/dom/GMavenSchemaProvider.kt
@@ -9,9 +9,11 @@ class GMavenSchemaProvider : StandardResourceProvider {
         if (MavenUtils.pluginEnabled(MavenUtils.INTELLIJ_MAVEN_PLUGIN_ID) || registrar == null) return
 
         mutableMapOf(
-            "http://maven.apache.org/xsd/maven-4.0.0.xsd" to "/schemas/maven-4.0.0.xsd",
-            "http://maven.apache.org/maven-v4_0_0.xsd" to "/schemas/maven-4.0.0.xsd",
-            "http://maven.apache.org/xsd/settings-1.2.0.xsd" to "/schemas/settings-1.2.0.xsd",
+            "http://maven.apache.org/xsd/maven-4.0.0.xsd" to "schemas/maven-4.0.0.xsd",
+            "https://maven.apache.org/xsd/maven-4.1.0.xsd" to "schemas/maven-4.1.0.xsd",
+            "http://maven.apache.org/maven-v4_0_0.xsd" to "schemas/maven-4.0.0.xsd",
+            "http://maven.apache.org/xsd/settings-1.1.0.xsd" to "schemas/settings-1.1.0.xsd",
+            "http://maven.apache.org/xsd/settings-1.2.0.xsd" to "schemas/settings-1.2.0.xsd",
         ).forEach { (url, path) -> addStdResource(registrar, url, path) }
     }
 
